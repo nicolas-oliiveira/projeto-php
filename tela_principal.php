c6bbe24c92c462bf579fce26b6abb316bc85b57d
nicolas-oliiveira <nicolas.oliveira115@etec.sp.gov.br>
2022-05-17 12:26:04 -0300

    ni

---
+++ tela_principal.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>tela principal</title>
+    </head>
+    <body>
+        <h1>Menu de opções </h1>
+        <a href="opcao.php?tela=01">TELA 01</a>&nbsp;
+        <a href="opcao.php?tela=02">TELA 02</a>&nbsp;
+        <a href="opcao.php?tela=03">TELA 03</a><br><br>
+
+        <h3>PAGINA INICIAL</h3>
+
+    </body>
+</html>
